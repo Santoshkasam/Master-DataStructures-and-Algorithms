07093ef49b063ccb94a8ceb3d6b27cd83b99d3df
Santosh Kumar Kasam <kasamsantosh34@yahoo.com>
2022-01-31 17:13:02 +0100

    Comments: Added comments that explain how an array is read as a parameter.

--- Concepts of C and C++/Array as parameter 1.cpp
+++ Concepts of C and C++/Array as parameter 1.cpp
@@ -7,12 +7,19 @@
 using namespace std;
 void func(int A[]) {
 	cout << sizeof(A) / sizeof(int) << endl;
+	// Here since A is a pointer the array, sizeof(A) returns size of the
+	// pointer, but not the array.
 
 }
 
 int main() {
 	int A[] = {2,4,6,8,10};
-	func(A);
+	func(A); // The address of the array is passed.
+             // Arrays cannot be passed by value
+
+    cout << sizeof(A)/sizeof(int) <<endl;
+    // Here, A is the array and not a pointer. Hence the sizeof(A) returns the size
+    // of the array.
 
 	return 0;
 }
